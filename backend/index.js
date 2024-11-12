d14c1a53c270f0ea79d6b27eebb38218bc44f82e
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2024-11-12 15:04:44 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,13 @@ const bodyParser = require('body-parser');
 const { generateResponse } = require('./bot'); 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(CORS());
 app.use(bodyParser.json());
 
 app.post('/chat', async (req, res) => {
   const userMessage = req.body.message;
-
 
   if (!userMessage || !userMessage.trim()) {
     return res.status(400).json({ error: 'Mensagem não pode ser vazia!' });
@@ -24,7 +23,7 @@ app.post('/chat', async (req, res) => {
       return res.json({ message: 'Desculpe, não consegui entender sua mensagem. Pode reformular?' });
     }
 
-    res.json({ message: botResponse.responseContent, roleResponse: botResponse.roleResponse  });
+    res.json({ message: botResponse.responseContent, roleResponse: botResponse.roleResponse });
   } catch (error) {
     console.error('Erro ao processar a mensagem:', error);
     res.status(500).json({ error: `Erro ao processar a mensagem: ${error.message}` });
